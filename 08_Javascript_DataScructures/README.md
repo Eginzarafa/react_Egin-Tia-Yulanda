564183239cdb6ff8a215326df6ced29aed0d181e
Egin Zarafa <eginyulanda@gmail.com>
2023-09-03 00:59:46 +0700

    Tugas Data Sctucture

---
+++ 08_Javascript_DataScructures/README.md
@@ -0,0 +1,19 @@
+Struktur data (Data Structure) adalah cara untuk menyimpan, mengorganisir, dan mengelola data dalam komputer. 
+
+Struktur data adalah cara untuk mengatur dan menyimpan data sehingga dapat diakses dan dimanipulasi dengan efisien. 
+
+Struktur data dirancang untuk mencapai beberapa tujuan utama, termasuk efisiensi, keterbacaan, keterorganisasian, dan kemudahan pemeliharaan kode.
+
+Jenis-Jenis Struktur Data:
+
+Array: Kumpulan elemen data dengan indeks numerik.
+Linked List: Sekumpulan simpul yang terhubung satu sama lain.
+Stack: Struktur data LIFO (Last-In-First-Out).
+Queue: Struktur data FIFO (First-In-First-Out).
+Tree: Struktur data hirarkis dengan akar dan cabang-cabang.
+Graph: Kumpulan simpul yang terhubung dengan tepi.
+
+Operasi Pada Struktur Data yaitu Struktur data biasanya mendukung operasi dasar seperti penambahan, penghapusan, pencarian, dan pemutakhiran elemen data.
+
+seperti halnya dengan tugas pada pertemuan ini yang mana saya menambahkan fitur menghapus dan mencari (search) data yang ada pada table 
+dengan begitu akan mempermudah pencarian data pada table atau lainnya yang berjumlah data yang banyak
---
+++ 08_Javascript_DataScructures/praktikum/script.js
@@ -0,0 +1,102 @@
+// Fungsi untuk menambahkan produk ke tabel
+function addProduct() {
+    // Mengambil nilai dari input form
+    let productName = document.getElementById('DataName').value;
+    let productCategory = document.getElementById('kategoti').value;
+    let image = document.getElementById('formFile').value;
+    let optionProduct = document.getElementById('firstCheckbox');
+    let optionProduct1 = document.getElementById('secondCheckbox');
+    let optionProduct2 = document.getElementById('thirdCheckbox');
+    let description = document.getElementById('floatingTextarea2').value;
+    let productPrice = document.getElementById('price').value;
+
+    // Mengambil tabel
+    let table = document.getElementById("bodyTable");
+
+    // Validasi Product Name
+    if (productName.length > 25) {
+        alert("Product Name must not exceed 25 characters.");
+        document.getElementById('DataName').style.borderColor = "red";
+        return; // Berhenti jika nama produk terlalu panjang
+    }
+
+    if (/[#@{}]/.test(productName)) {
+        alert("Name must not contain symbols.");
+        document.getElementById('DataName').style.borderColor = "red";
+        return; // Berhenti jika ada simbol dalam nama produk
+    }
+
+    if (productName.trim() === "") {
+        alert("Please enter a valid Product name.");
+        document.getElementById('DataName').style.borderColor = "red";
+        return; // Berhenti jika nama produk kosong
+    }
+
+    // Validasi Product Price
+    if (productPrice.trim() === "" || isNaN(productPrice)) {
+        alert("Please enter a valid Product price.");
+        document.getElementById('price').style.borderColor = "red";
+        return; // Berhenti jika harga produk kosong atau bukan angka
+    }
+
+    // Buat baris baru dan tambahkan data ke tabel
+    let newRow = table.insertRow(table.rows.length);
+    newRow.insertCell(0).innerHTML = table.rows.length - 1;
+    newRow.insertCell(1).innerHTML = productName;
+    newRow.insertCell(2).innerHTML = productCategory;
+    newRow.insertCell(3).innerHTML = image;
+    
+    // Menyimpan nilai yang dipilih dari checkbox
+    let freshness = '';
+    if (optionProduct.checked) {
+        freshness = optionProduct.value;
+    } else if (optionProduct1.checked) {
+        freshness = optionProduct1.value;
+    } else if (optionProduct2.checked) {
+        freshness = optionProduct2.value;
+    }
+    
+    newRow.insertCell(4).innerHTML = freshness;
+    newRow.insertCell(5).innerHTML = description;
+    newRow.insertCell(6).innerHTML = productPrice;
+    newRow.insertCell(7).innerHTML = "<button onclick='removeProduct(this)'>Deleted</button>";
+
+    // Reset input setelah menambahkan data
+    document.getElementById('DataName').value = "";
+    document.getElementById('kategoti').value = "Choose";
+    document.getElementById('formFile').value = "";
+    optionProduct.checked = false;
+    optionProduct1.checked = false;
+    optionProduct2.checked = false;
+    document.getElementById('floatingTextarea2').value = "";
+    document.getElementById('price').value = "";
+
+    // Tambahkan event listener pada elemen pencarian
+    document.getElementById("searchInput").addEventListener("keyup", searchProduct);
+}
+
+// Fungsi untuk menghapus produk dari tabel
+function removeProduct(button) {
+    let row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+// Fungsi untuk mencari produk dalam tabel
+function searchProduct() {
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("outputTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 1; i < tr.length; i++) { // Mulai dari 1 untuk melewati baris header tabel
+        td = tr[i].getElementsByTagName("td")[1]; // Ganti 1 dengan indeks kolom yang ingin Anda cari
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = ""; // Tampilkan baris jika kata kunci cocok
+            } else {
+                tr[i].style.display = "none"; // Sembunyikan baris jika tidak cocok
+            }
+        }
+    }
+}
